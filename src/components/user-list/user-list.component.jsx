3df27fedb77bad9480dc2f32d2157eac508d7d24
Aensheng <aensheng@gmail.com>
2019-10-13 04:31:42 +1100

    data transfer from HOC to component

---
+++ src/components/user-list/user-list.component.jsx
@@ -0,0 +1,18 @@
+import withData from '../../with-data'
+import React from 'react'
+
+const UserList = ({ data }) => (
+    <div className='container user-list'>
+        <h1> User List </h1>
+        {
+            data.map(user => (
+                <div className='post' key={user.id}>
+                    <h1> {user.name} </h1>
+                    <h2> {user.email} </h2>
+                </div>
+            ))
+        }
+    </div>
+)
+
+export default withData(UserList)
---
+++ src/with-data.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const withData = WrappedComponent => {
+    class WithData extends React.Component {
+        // get props from WrappedComponent
+        constructor(props) {
+            super(props)
+
+            this.state = {
+                data : []
+            }
+        }
+
+        // get real data from the url provided by WrappedComponent's props
+        componentDidMount() {
+            setTimeout(()=>{
+                fetch(this.props.dataSource)
+                    .then(response => response.json())
+                    .then(data => this.setState({ data: data.slice(0,3)}))
+            }, 1500)
+        }
+
+        // return data back
+        render() {
+            const { dataSource, ...otherProps } = this.props
+
+            return this.state.data.length < 1 ? (
+                <h1>Loading</h1>
+            ) : (
+                <WrappedComponent data = {this.state.data} {...otherProps} />
+            )
+        }
+    }
+
+    return WithData
+}
+
+export default withData
